069cbbb2f0d12f57b3dd1aa5a19368b4cce648c1
Ajaykumar <67772875+aubyo1522@users.noreply.github.com>
2023-11-04 10:11:09 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlidComponent } from './slid/slid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlidComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/slid/slid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slid',
+  templateUrl: './slid.component.html',
+  styleUrls: ['./slid.component.css']
+})
+export class SlidComponent {
+
+}
